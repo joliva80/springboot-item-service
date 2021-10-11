fcea5392d744f43422d95b1c562881d8a603ae26
Joan Oliva <joanoj@gmail.com>
2021-10-11 19:33:37 +0200

    circuit breaker resilience4j

--- src/main/java/com/springboot/app/item/AppConfig.java
+++ src/main/java/com/springboot/app/item/AppConfig.java
@@ -1,8 +1,16 @@
 package com.springboot.app.item;
 
+//import java.time.Duration;
+
+//import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+//import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+//import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+//import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+//import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -11,4 +19,24 @@ public class AppConfig {
     public RestTemplate registerRestTemplate(){
         return new RestTemplate();
     }
+
+    /*
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> configDefaultCustomizer(){
+        return factory -> factory.configureDefault(id -> {  // for each circuit break identified by "id"
+            return new Resilience4JConfigBuilder(id)        // configure the circuite break for the given "id" circuit break
+                        .circuitBreakerConfig(CircuitBreakerConfig.custom() // Only for the given config: circuitbreak, then set the config parameters
+                                .slidingWindowSize(10)
+                                .failureRateThreshold(50)
+                                .waitDurationInOpenState(Duration.ofMillis(10000))
+                                .build())
+                        //.timeLimiterConfig(TimeLimiterConfig.ofDefaults()) // ofDefaults is by defaults, or .custom() to customize.
+                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                .timeoutDuration(Duration.ofSeconds(6L))
+                                .build())  // timeout to 2 seconds response max time
+                        .build();
+        });
+    }
+    */
+
 }
--- src/main/java/com/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/springboot/app/item/controller/ItemController.java
@@ -1,22 +1,30 @@
 package com.springboot.app.item.controller;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import com.springboot.app.item.model.Item;
 import com.springboot.app.item.model.service.ItemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 
 @RestController
 public class ItemController {
     
+    //@Autowired
+    //private CircuitBreakerFactory cbFactory;
+
     @Autowired
     @Qualifier("feignService") // Select the bean for the implementation, just in case there is no one by default. This is optional.
     private ItemService itemService;
@@ -29,8 +37,30 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    /*
+    // Circuit break with coding CircuitBreakFactory library
     @GetMapping("/items/{id}/quantity={quantity}")
     public Item getItemsById(@PathVariable Long id, @PathVariable Integer quantity){
+        //return itemService.findById(id, quantity);
+        return cbFactory.create("ItemsCircuitBreaker")  // cbItems is the name we have set for the Items Circuit Breaker
+                        .run(() -> itemService.findById(id, quantity));
+    }
+    */
+
+    /*
+    // Circuit break with annotations
+    @CircuitBreaker(name="ItemsCircuitBreaker") // , fallbackmethod()) alternative method in case of open circuit
+    @GetMapping("/items/{id}/quantity={quantity}")
+    public Item getItemsByIdWithCircuitBreakAnnotation(@PathVariable Long id, @PathVariable Integer quantity){
         return itemService.findById(id, quantity);
+    }
+    */
+
+    // Timeout + CircuitBreak with annotations
+    @TimeLimiter(name="ItemsCircuitBreaker")
+    @CircuitBreaker(name="ItemsCircuitBreaker") // , fallbackmethod()) alternative method in case of open circuit
+    @GetMapping("/items/{id}/quantity={quantity}")
+    public CompletableFuture<Item> getItemsByIdWithCircuitBreakAnnotation(@PathVariable Long id, @PathVariable Integer quantity){
+        return CompletableFuture.supplyAsync(() -> itemService.findById(id, quantity)) ;
     }
 }
